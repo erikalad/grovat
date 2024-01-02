2b679db82bc6b30fd0ac6816a4f4c7992a84cc83
Erika Ladner <erikaladner@MacBook-Air-de-Erika.local>
2024-01-02 19:29:52 -0300

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import Menu from "./Componentes/Menu"
